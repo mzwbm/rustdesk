265b1db4cf84b02ed7e4976d1eedd1811ef7f7d4
mzwbm <63568863+mzwbm@users.noreply.github.com>
2024-10-09 17:21:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["mzwbm.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "C3+ms2e6cb5xgZMbUKkXhAPOVsi5HQ0G1wKSFvTVm98=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
