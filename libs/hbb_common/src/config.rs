8363f8243ea826dc598f4c10df68820462501bd6
mzwbm <63568863+mzwbm@users.noreply.github.com>
2024-10-09 18:52:24 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["mzwbm.top"];
-pub const PUBLIC_RS_PUB_KEY: &str = "C3+ms2e6cb5xgZMbUKkXhAPOVsi5HQ0G1wKSFvTVm98=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["mzbm.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "C35HQ0G1wSFvTVm98=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
